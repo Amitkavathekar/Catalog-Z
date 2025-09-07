7c4547ea2f84f0e8c08ffdf89566558403660713
Amit Kavathekar <amitkavathekar123@gmail.com>
2025-09-07 12:07:38 +0530

    changes in app routing

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Photos from '../pages/Photos'
+import React from "react";
+import Photos from "../pages/Photos";
 
 const AppRoutes = () => {
   return (
-<Routes>
-    <Route path="/" element={<Photos/>}></Route>    
-    <Route path="" element={}></Route>    
-    <Route path="" element={}></Route>    
-</Routes>  )
-}
+    <Routes>
+      <Route path="/" element={<Photos />}></Route>
+    </Routes>
+  );
+};
 
-export default AppRoutes
+export default AppRoutesq;
