3c9a665a35dbbeb1f74076fbaa727b5c496e5165
Amit Kavathekar <amitkavathekar123@gmail.com>
2025-09-07 11:58:20 +0530

    intial commit

---
+++ src/Routes/AppRoutes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Photos from '../pages/Photos'
+
+const AppRoutes = () => {
+  return (
+<Routes>
+    <Route path="/" element={<Photos/>}></Route>    
+    <Route path="" element={}></Route>    
+    <Route path="" element={}></Route>    
+</Routes>  )
+}
+
+export default AppRoutes
